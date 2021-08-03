e2276dccc361b204333fe6ca7fded37f7813e027
j6847110 <j6847110@naver.com>
2021-08-03 19:16:37 +0900

    req,res

--- src/server.js
+++ src/server.js
@@ -4,6 +4,17 @@ const PORT = 4000;
 
 const app = express();
 
+const handleHome = (req, res) => {
+  return res.send("I still love you");
+};
+
+const handleLogin = (req, res) => {
+  return res.send("I login");
+};
+
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+
 const handleListening = () =>
   console.log(`Server Listening on http://localhost:${PORT}`);
 
