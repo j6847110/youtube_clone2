94638bacd21f8f24b09a7e553d9c22a1a072daab
j6847110 <j6847110@naver.com>
2021-08-04 23:28:37 +0900

    8.4

---
+++ README.md
@@ -0,0 +1,15 @@
+#
+
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+/users/edit -> Edit user
+/users/delete
+
+/videos/watch
+/videos/edit
+/videos/delete
+/videos/comments
+/videos/comments/delete
--- src/server.js
+++ src/server.js
@@ -1,19 +1,29 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
 
-const handleHome = (req, res) => {
-  return res.send("I still love you");
-};
+const logger = morgan("dev");
+app.use(logger);
 
-const handleLogin = (req, res) => {
-  return res.send("I login");
-};
+const globalRouter = express.Router();
+const handleHome = (req, res) => res.send("Home");
+globalRouter.get("/", handleHome);
+const userRouter = express.Router();
+const handleEditUser = (req, res) => res.send("Edit User");
 
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+userRouter.get("/edit", handleEditUser);
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
   console.log(`Server Listening on http://localhost:${PORT}`);
