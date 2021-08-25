1087e0ac8dd2d04fefea16d3f0a60bce92473690
j6847110 <j6847110@naver.com>
2021-08-25 23:15:11 +0900

    login first

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,8 @@
-const join = (req, res) => res.send("join");
+const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
+const postJoin = (req, res) => {
+  console.log(req.body);
+  res.end();
+};
 const edit = (req, res) => res.send("edit");
 const remove = (req, res) => res.send("remove user");
 const login = (req, res) => res.send("login");
@@ -6,4 +10,4 @@ const search = (req, res) => res.send("search");
 const logout = (req, res) => res.send("Log out");
 const see = (req, res) => res.send("see User");
 
-export { join, edit, remove, login, search, see, logout };
+export { getJoin, postJoin, edit, remove, login, search, see, logout };
